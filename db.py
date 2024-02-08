ecc814589980439f280f5fe1555a9f2f77d28410
elahe amini <elaheamini.0303@gmail.com>
2024-02-08 22:28:44 +0330

    ADD mobile column

--- db.py
+++ db.py
@@ -1,14 +1,3 @@
-# import sqlite3
-# [1:05 PM]
-# conn = sqlite3.connect("esme_db.db")
-# [1:05 PM]
-# cursor = conn.cursor()
-# [1:06 PM]
-# cursor.execute("SQL QUERY")
-# [1:07 PM]
-# conn.commit()
-# cursor.close()
-# conn.close()
 import sqlite3
 
 
@@ -20,6 +9,7 @@ def create_table():
                    lastname VARCHAR(31) NOT NULL,
                    shomare_kart INT NOT NULL UNIQUE,
                    password INT NOT NULL,
+                   mobile VARCHAR(11) NOT NULL,
                    etebar INT NOT NULL DEFAULT 100,
                    charge INT NOT NULL DEFAULT 0
                    );""")
@@ -27,15 +17,14 @@ def create_table():
     cursor.close()
     conn.close()
 
-def new_user(firstname: str, lastname: str, shomare_kart: int, password: int):
+def new_user(firstname: str, lastname: str, shomare_kart: int, password: int, mobile: str):
     conn = sqlite3.connect("up.db")
     cursor = conn.cursor()
-    cursor.execute("""INSERT INTO main (firstname,lastname,shomare_kart,password)
-                   VALUES (?, ?, ?, ?);""",(firstname, lastname, shomare_kart, password))
+    cursor.execute("""INSERT INTO main (firstname, lastname, shomare_kart, password, mobile)
+                   VALUES (?, ?, ?, ?, ?);""",(firstname, lastname, shomare_kart, password, mobile))
     conn.commit()
     cursor.close()
     conn.close()
 
-#new_user(firstname="elahe",lastname="amini",shomare_kart=6037997503297156,password=1234)
-create_table()
-    
+# new_user(firstname="elahe",lastname="amini",shomare_kart=6037997503297156,password=1234, mobile="09120000000")
+# create_table()
