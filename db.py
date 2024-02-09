80ae985a8300054b83dadf8b4303a86abd1d7fc3
elahe amini <elaheamini.0303@gmail.com>
2024-02-09 14:53:00 +0330

    ADD check_password function without details

--- db.py
+++ db.py
@@ -36,14 +36,14 @@ def get_user_by_name(firstname: str, lastname: str):
     conn.close()
     return result
 
-def transaction(sender_card: int, receiver_card: int, amount: int, password: str): #without using password
-    # check if amount is allowed
+def transaction(sender_card: int, receiver_card: int, amount: int, password: str):
     conn = sqlite3.connect("up.db")
     cursor = conn.cursor()
     cursor.execute(f"SELECT password FROM main WHERE shomare_kart = {sender_card};")
     if password==cursor.fetchone()[0]:
         cursor.execute(f"SELECT etebar FROM main WHERE shomare_kart = {sender_card};")
         sender_etebar = cursor.fetchone()[0]
+        # check if amount is allowed
         if amount <= sender_etebar:
             new_sender_etebar = sender_etebar - amount
             cursor.execute(f"SELECT etebar FROM main WHERE shomare_kart = {receiver_card};")
@@ -68,7 +68,7 @@ def transaction(sender_card: int, receiver_card: int, amount: int, password: str
             cursor.close()
             conn.close()
 
-def to_charge(simcard: str, mablagh: int): #without using password
+def to_charge(simcard: str, mablagh: int): #without checking password
     conn = sqlite3.connect("up.db")
     cursor = conn.cursor()
     cursor.execute(f"SELECT column_mobile FROM table_main WHERE EXISTS {simcard};") #how to check if a data exists
@@ -97,7 +97,27 @@ def to_charge(simcard: str, mablagh: int): #without using password
             conn.close()
             raise Exception("NOT ENOUGH ETEBAR!")
 
+
 #to_charge('09123456789', 50)
 
+def check_password(shomare_kart: int, password: str):
+    conn = sqlite3.connect("up.db")
+    cursor = conn.cursor()
+    cursor.execute(f"SELECT password FROM main WHERE shomare_kart = {shomare_kart};")
+    password_enterd=cursor.fetchone()[0]
+    if password==password_enterd:
+        print("yeessss")
 
-    
+    else:
+        while password!=password_enterd:
+            password=input("incorrect password. try again or enter 4 to back to menu.") 
+            #if password=="4":
+                #pass #add back to menu option later
+        check_password(shomare_kart, password)
+        #print("incorrect")
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+
+#check_password(6087456734569876, "0778")
