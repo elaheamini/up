3ff0063c943f3465fc279f6ecf67a28fd7629b8a
elahe amini <elaheamini.0303@gmail.com>
2024-02-09 16:04:31 +0330

    ADD key 2 to menu by using transaction function

--- menu.py
+++ menu.py
@@ -1,10 +1,14 @@
 import db
 while True:
     keyboard=input("adad")
-    def keyboard1():
-        if keyboard=="1":
+    
+    if keyboard=="1":
             firstname, lastname, shomare_kart, password, mobile = input().split()
             db.new_user(firstname, lastname, shomare_kart, password, mobile)
             #if each person has only one mobile number
-    continue
-        #elif keyboard=="2"
+        
+
+    elif keyboard=="2":
+          sender_card, receiver_card, amount, password=input().split()
+          amount=int(amount)
+          db.transaction(sender_card, receiver_card, amount, password)
