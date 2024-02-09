a1284f99b3f4df63020d1bd0bee4ee4ac5032cef
elahe amini <elaheamini.0303@gmail.com>
2024-02-09 17:38:58 +0330

    ADD key 3 to menu by using to_charge function

--- menu.py
+++ menu.py
@@ -6,9 +6,13 @@ while True:
             firstname, lastname, shomare_kart, password, mobile = input().split()
             db.new_user(firstname, lastname, shomare_kart, password, mobile)
             #if each person has only one mobile number
-        
 
     elif keyboard=="2":
           sender_card, receiver_card, amount, password=input().split()
           amount=int(amount)
           db.transaction(sender_card, receiver_card, amount, password)
+
+    elif keyboard=="3":
+          simcard, mablagh=input().split()
+          mablagh=int(mablagh)
+          db.to_charge(simcard, mablagh)
